78bf4dc81917c4f9ca402ef34f48d1be202824d8
LucaBarbaLata <Gamer@DESKTOP-CTFPEVB>
2024-06-20 14:54:53 +0300

    lychee

--- src/main/java/net/mcreator/fuzzy/item/LycheeItem.java
+++ src/main/java/net/mcreator/fuzzy/item/LycheeItem.java
@@ -1,15 +1,11 @@
 
 package net.mcreator.fuzzy.item;
 
-import net.minecraft.world.level.Level;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.LivingEntity;
 
 public class LycheeItem extends Item {
 	public LycheeItem() {
@@ -24,20 +20,5 @@ public class LycheeItem extends Item {
 	@Override
 	public int getUseDuration(ItemStack itemstack) {
 		return 0;
-	}
-
-	@Override
-	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
-		ItemStack retval = new ItemStack(Items.ENDER_PEARL);
-		super.finishUsingItem(itemstack, world, entity);
-		if (itemstack.isEmpty()) {
-			return retval;
-		} else {
-			if (entity instanceof Player player && !player.getAbilities().instabuild) {
-				if (!player.getInventory().add(retval))
-					player.drop(retval, false);
-			}
-			return itemstack;
-		}
 	}
 }
