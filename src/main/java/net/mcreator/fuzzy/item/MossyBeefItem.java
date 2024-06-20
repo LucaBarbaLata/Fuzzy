99c39fcfc61ad75c44c566b16463b7e2e2682a8b
DenisGithubWabibi <denis@DESKTOP-ODASD81>
2024-06-20 18:26:06 +0300

    few changes to beef

--- src/main/java/net/mcreator/fuzzy/item/MossyBeefItem.java
+++ src/main/java/net/mcreator/fuzzy/item/MossyBeefItem.java
@@ -2,11 +2,17 @@
 package net.mcreator.fuzzy.item;
 
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.food.FoodProperties;
 
 public class MossyBeefItem extends Item {
 	public MossyBeefItem() {
-		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(2).saturationMod(0.1f).alwaysEat().meat().build()));
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(3).saturationMod(0.3f).alwaysEat().meat().build()));
+	}
+
+	@Override
+	public int getUseDuration(ItemStack itemstack) {
+		return 3;
 	}
 }
