b8458bc65b993e2dd58ab461fcc88ad2e7786e49
DenisGithubWabibi <denis@DESKTOP-ODASD81>
2024-06-20 18:42:45 +0300

    added mob eggs in fuzzy tab

--- src/main/java/net/mcreator/fuzzy/init/FuzzyModTabs.java
+++ src/main/java/net/mcreator/fuzzy/init/FuzzyModTabs.java
@@ -6,20 +6,15 @@ package net.mcreator.fuzzy.init;
 
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.Registries;
 
 import net.mcreator.fuzzy.FuzzyMod;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class FuzzyModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, FuzzyMod.MODID);
 	public static final RegistryObject<CreativeModeTab> FUZZY = REGISTRY.register("fuzzy",
@@ -33,18 +28,12 @@ public class FuzzyModTabs {
 				tabData.accept(FuzzyModItems.STARRY_ARMOR_LEGGINGS.get());
 				tabData.accept(FuzzyModItems.STARRY_ARMOR_BOOTS.get());
 				tabData.accept(FuzzyModItems.LYCHEE.get());
+				tabData.accept(FuzzyModItems.BIANAL_SPAWN_EGG.get());
+				tabData.accept(FuzzyModItems.LABUSHA_SPAWN_EGG.get());
+				tabData.accept(FuzzyModItems.MOSSHEEP_SPAWN_EGG.get());
 				tabData.accept(FuzzyModItems.MOSSY_BEEF.get());
 				tabData.accept(FuzzyModItems.BLUE_APPLE.get());
 			})
 
 					.build());
-
-	@SubscribeEvent
-	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
-		if (tabData.getTabKey() == CreativeModeTabs.SPAWN_EGGS) {
-			tabData.accept(FuzzyModItems.BIANAL_SPAWN_EGG.get());
-			tabData.accept(FuzzyModItems.LABUSHA_SPAWN_EGG.get());
-			tabData.accept(FuzzyModItems.MOSSHEEP_SPAWN_EGG.get());
-		}
-	}
 }
