f08a69e43ce8b414b64373a53edbbcbc703236f7
LucaBarbaLata <User@Lavinia>
2024-06-20 14:17:39 +0300

    Added Strawberrys

--- src/main/java/net/mcreator/fuzzy/init/FuzzyModItems.java
+++ src/main/java/net/mcreator/fuzzy/init/FuzzyModItems.java
@@ -10,6 +10,7 @@ import net.minecraftforge.registries.DeferredRegister;
 
 import net.minecraft.world.item.Item;
 
+import net.mcreator.fuzzy.item.StrawberrysItem;
 import net.mcreator.fuzzy.item.CutiItem;
 import net.mcreator.fuzzy.item.CuteCreaturesItem;
 import net.mcreator.fuzzy.FuzzyMod;
@@ -18,6 +19,7 @@ public class FuzzyModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, FuzzyMod.MODID);
 	public static final RegistryObject<Item> CUTI_BUCKET = REGISTRY.register("cuti_bucket", () -> new CutiItem());
 	public static final RegistryObject<Item> CUTE_CREATURES = REGISTRY.register("cute_creatures", () -> new CuteCreaturesItem());
+	public static final RegistryObject<Item> STRAWBERRYS = REGISTRY.register("strawberrys", () -> new StrawberrysItem());
 	// Start of user code block custom items
 	// End of user code block custom items
 }
---
+++ src/main/java/net/mcreator/fuzzy/item/StrawberrysItem.java
@@ -0,0 +1,12 @@
+
+package net.mcreator.fuzzy.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.food.FoodProperties;
+
+public class StrawberrysItem extends Item {
+	public StrawberrysItem() {
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.RARE).food((new FoodProperties.Builder()).nutrition(5).saturationMod(5f).alwaysEat().build()));
+	}
+}
