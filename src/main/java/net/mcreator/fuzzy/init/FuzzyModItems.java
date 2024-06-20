c25f83e8ce9b73f2bfe51dce383b94a66130f05b
LucaBarbaLata <Gamer@DESKTOP-CTFPEVB>
2024-06-20 14:18:37 +0300

    ciroza

--- src/main/java/net/mcreator/fuzzy/init/FuzzyModItems.java
+++ src/main/java/net/mcreator/fuzzy/init/FuzzyModItems.java
@@ -7,15 +7,32 @@ package net.mcreator.fuzzy.init;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.api.distmarker.Dist;
 
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Item;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.item.ItemProperties;
 
 import net.mcreator.fuzzy.item.CutiItem;
+import net.mcreator.fuzzy.item.CirozaItem;
 import net.mcreator.fuzzy.FuzzyMod;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class FuzzyModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, FuzzyMod.MODID);
 	public static final RegistryObject<Item> CUTI_BUCKET = REGISTRY.register("cuti_bucket", () -> new CutiItem());
+	public static final RegistryObject<Item> CIROZA = REGISTRY.register("ciroza", () -> new CirozaItem());
+
 	// Start of user code block custom items
 	// End of user code block custom items
+	@SubscribeEvent
+	public static void clientLoad(FMLClientSetupEvent event) {
+		event.enqueueWork(() -> {
+			ItemProperties.register(CIROZA.get(), new ResourceLocation("blocking"), ItemProperties.getProperty(Items.SHIELD, new ResourceLocation("blocking")));
+		});
+	}
 }
---
+++ src/main/java/net/mcreator/fuzzy/item/CirozaItem.java
@@ -0,0 +1,11 @@
+
+package net.mcreator.fuzzy.item;
+
+import net.minecraft.world.item.ShieldItem;
+import net.minecraft.world.item.Item;
+
+public class CirozaItem extends ShieldItem {
+	public CirozaItem() {
+		super(new Item.Properties().durability(100).fireResistant());
+	}
+}
