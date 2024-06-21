311544500d251b7d2f35112ddd5958687f8f5e75
LucaBarbaLata <User@Lavinia>
2024-06-21 11:15:42 +0300

    Moved the armors in the Fuzzy Creative Tab

--- src/main/java/net/mcreator/fuzzy/init/FuzzyModTabs.java
+++ src/main/java/net/mcreator/fuzzy/init/FuzzyModTabs.java
@@ -6,20 +6,15 @@ package net.mcreator.fuzzy.init;
 
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.Registries;
 
 import net.mcreator.fuzzy.FuzzyMod;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class FuzzyModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, FuzzyMod.MODID);
 	public static final RegistryObject<CreativeModeTab> FUZZY = REGISTRY.register("fuzzy",
@@ -49,21 +44,15 @@ public class FuzzyModTabs {
 				tabData.accept(FuzzyModItems.MOSSY_STEAK.get());
 				tabData.accept(FuzzyModItems.PENCIL.get());
 				tabData.accept(FuzzyModItems.OI_OI_OI.get());
+				tabData.accept(FuzzyModItems.APPLE_ARMOR_HELMET.get());
+				tabData.accept(FuzzyModItems.APPLE_ARMOR_CHESTPLATE.get());
+				tabData.accept(FuzzyModItems.APPLE_ARMOR_LEGGINGS.get());
+				tabData.accept(FuzzyModItems.APPLE_ARMOR_BOOTS.get());
+				tabData.accept(FuzzyModItems.BLUE_ARMOR_HELMET.get());
+				tabData.accept(FuzzyModItems.BLUE_ARMOR_CHESTPLATE.get());
+				tabData.accept(FuzzyModItems.BLUE_ARMOR_LEGGINGS.get());
+				tabData.accept(FuzzyModItems.BLUE_ARMOR_BOOTS.get());
 			})
 
 					.build());
-
-	@SubscribeEvent
-	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
-		if (tabData.getTabKey() == CreativeModeTabs.COMBAT) {
-			tabData.accept(FuzzyModItems.APPLE_ARMOR_HELMET.get());
-			tabData.accept(FuzzyModItems.APPLE_ARMOR_CHESTPLATE.get());
-			tabData.accept(FuzzyModItems.APPLE_ARMOR_LEGGINGS.get());
-			tabData.accept(FuzzyModItems.APPLE_ARMOR_BOOTS.get());
-			tabData.accept(FuzzyModItems.BLUE_ARMOR_HELMET.get());
-			tabData.accept(FuzzyModItems.BLUE_ARMOR_CHESTPLATE.get());
-			tabData.accept(FuzzyModItems.BLUE_ARMOR_LEGGINGS.get());
-			tabData.accept(FuzzyModItems.BLUE_ARMOR_BOOTS.get());
-		}
-	}
 }
