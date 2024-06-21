d26810812b1dd97d4aafd4b1052736ca10fccacb
LucaBarbaLata <User@Lavinia>
2024-06-21 10:28:54 +0300

    Removed Advancment Blue Apple Now Grants Speed After Eating

--- src/main/java/net/mcreator/fuzzy/item/BlueAppleItem.java
+++ src/main/java/net/mcreator/fuzzy/item/BlueAppleItem.java
@@ -1,12 +1,27 @@
 
 package net.mcreator.fuzzy.item;
 
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.entity.LivingEntity;
+
+import net.mcreator.fuzzy.procedures.BlueApplePlayerFinishesUsingItemProcedure;
 
 public class BlueAppleItem extends Item {
 	public BlueAppleItem() {
 		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(3).saturationMod(0.3f).alwaysEat().build()));
+	}
+
+	@Override
+	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
+		ItemStack retval = super.finishUsingItem(itemstack, world, entity);
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		BlueApplePlayerFinishesUsingItemProcedure.execute(entity);
+		return retval;
 	}
 }
