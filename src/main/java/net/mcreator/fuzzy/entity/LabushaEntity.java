bc53503f7530352115a58e607341c176e85ca8f3
DenisGithubWabibi <denis@DESKTOP-ODASD81>
2024-06-21 09:57:02 +0300

    changed speed of mossy cow and corrected the mistake to labusha

--- src/main/java/net/mcreator/fuzzy/entity/LabushaEntity.java
+++ src/main/java/net/mcreator/fuzzy/entity/LabushaEntity.java
@@ -125,7 +125,7 @@ public class LabushaEntity extends PathfinderMob {
 
 	public static AttributeSupplier.Builder createAttributes() {
 		AttributeSupplier.Builder builder = Mob.createMobAttributes();
-		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.3);
+		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.5);
 		builder = builder.add(Attributes.MAX_HEALTH, 10);
 		builder = builder.add(Attributes.ARMOR, 0);
 		builder = builder.add(Attributes.ATTACK_DAMAGE, 3);
--- src/main/java/net/mcreator/fuzzy/entity/MossheepEntity.java
+++ src/main/java/net/mcreator/fuzzy/entity/MossheepEntity.java
@@ -178,7 +178,7 @@ public class MossheepEntity extends Animal {
 
 	public static AttributeSupplier.Builder createAttributes() {
 		AttributeSupplier.Builder builder = Mob.createMobAttributes();
-		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.7);
+		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.3);
 		builder = builder.add(Attributes.MAX_HEALTH, 20);
 		builder = builder.add(Attributes.ARMOR, 0);
 		builder = builder.add(Attributes.ATTACK_DAMAGE, 0);
